5e30ffa3609dd7b9ec54621988df809a531a4533
松本 和馬 <hello@mtmtkzm.com>
2016-10-16 23:04:11 +0900

    aboutのページ

--- app/source/js/app.js
+++ app/source/js/app.js
@@ -1,5 +1,9 @@
-
-
+/* 下からタレが上がってくるやつ */
 // $('.js-tare').animate({
 //   'top': '10vh'
-// },30000) ;
+// },30000);
+
+/* ヘッダーメニューのToggle */
+$('.js-gnav-toggle').on('click', function() {
+  $(this).toggleClass('is-open');
+});
