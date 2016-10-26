e727854200e274477562236e6609f4eef813d429
松本 和馬 <hello@mtmtkzm.com>
2016-10-27 01:22:22 +0900

    サーバ直接いじったもろもろ

--- app/source/js/app.js
+++ app/source/js/app.js
@@ -18,8 +18,10 @@ var wPosX = (window.innerWidth-w)/2,
 
 function twitterPopup(){
 	var url = 'https://twitter.com/share?';
-	url += 'text=' + encodeURI('模擬店「TOLIC」 のクーポンを獲得しました！ pic.twitter.com/pUlrK0y1Im');
-	url += '&url=' + encodeURI('http://tolic.club/');
+	url += 'text=' + encodeURI('模擬店「TOLIC」のクーポンを獲得しました！ pic.twitter.com/b0rKXCL9u3');
+  url += '&url=' + '..';
+  url += '&via=' + 'TOLIC2016';
 	url += '&hashtags=' + '芸工祭2016';
+  console.log(url);
 	window.open(url, 'tweetwin', 'width='+w+',height='+h+',left='+wPosX+',top='+wPosY+',menubar=no,toolbar=no,scrollbars=yes');
 }
